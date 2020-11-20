3882a71c81fc900c03b0a9d39a2d180bec9a8d08
yeongbba <yeongmolee2@gmail.com>
2020-11-20 21:41:40 +0900

    finish chatbot

--- client/src/Chatbot/Chatbot.js
+++ client/src/Chatbot/Chatbot.js
@@ -1,7 +1,18 @@
 import React, { useEffect } from "react";
 import Axios from "axios";
-
+import { useDispatch, useSelector } from "react-redux";
+import { saveMessage } from "../_actions/message_actions";
+import Message from "./Sections/Message";
+import { List, Icon, Avatar } from "antd";
+import Card from "./Sections/Card";
 function Chatbot() {
+  const dispatch = useDispatch();
+  const messagesFromRedux = useSelector((state) => state.message.messages);
+
+  useEffect(() => {
+    eventQuery("welcomeToMyWebsite");
+  }, []);
+
   const textQuery = async (text) => {
     //  First  Need to  take care of the message I sent
     let conversation = {
@@ -12,6 +23,9 @@ function Chatbot() {
         },
       },
     };
+
+    dispatch(saveMessage(conversation));
+    // console.log('text I sent', conversation)
 
     // We need to take care of the message Chatbot sent
     const textQueryVariables = {
@@ -29,6 +43,8 @@ function Chatbot() {
           who: "bot",
           content: content,
         };
+
+        dispatch(saveMessage(conversation));
       }
     } catch (error) {
       conversation = {
@@ -39,6 +55,40 @@ function Chatbot() {
           },
         },
       };
+
+      dispatch(saveMessage(conversation));
+    }
+  };
+
+  const eventQuery = async (event) => {
+    // We need to take care of the message Chatbot sent
+    const eventQueryVariables = {
+      event,
+    };
+    try {
+      //I will send request to the textQuery ROUTE
+      const response = await Axios.post(
+        "/api/dialogflow/eventQuery",
+        eventQueryVariables
+      );
+      for (let content of response.data.fulfillmentMessages) {
+        let conversation = {
+          who: "bot",
+          content: content,
+        };
+
+        dispatch(saveMessage(conversation));
+      }
+    } catch (error) {
+      let conversation = {
+        who: "bot",
+        content: {
+          text: {
+            text: " Error just occured, please check the problem",
+          },
+        },
+      };
+      dispatch(saveMessage(conversation));
     }
   };
 
@@ -55,6 +105,54 @@ function Chatbot() {
     }
   };
 
+  const renderCards = (cards) => {
+    return cards.map((card, index) => (
+      <Card key={index} cardInfo={card.structValue} />
+    ));
+  };
+
+  const renderOneMessage = (message, i) => {
+    console.log("message", message);
+
+    // we need to give some condition here to separate message kinds
+
+    // template for normal text
+    if (message.content && message.content.text && message.content.text.text) {
+      return (
+        <Message key={i} who={message.who} text={message.content.text.text} />
+      );
+    } else if (message.content && message.content.payload.fields.card) {
+      const AvatarSrc =
+        message.who === "bot" ? <Icon type="robot" /> : <Icon type="smile" />;
+
+      return (
+        <div>
+          <List.Item style={{ padding: "1rem" }}>
+            <List.Item.Meta
+              avatar={<Avatar icon={AvatarSrc} />}
+              title={message.who}
+              description={renderCards(
+                message.content.payload.fields.card.listValue.values
+              )}
+            />
+          </List.Item>
+        </div>
+      );
+    }
+
+    // template for card message
+  };
+
+  const renderMessage = (returnedMessages) => {
+    if (returnedMessages) {
+      return returnedMessages.map((message, i) => {
+        return renderOneMessage(message, i);
+      });
+    } else {
+      return null;
+    }
+  };
+
   return (
     <div
       style={{
@@ -64,7 +162,9 @@ function Chatbot() {
         borderRadius: "7px",
       }}
     >
-      <div style={{ height: 644, width: "100%", overflow: "auto" }}>chat</div>
+      <div style={{ height: 644, width: "100%", overflow: "auto" }}>
+        {renderMessage(messagesFromRedux)}
+      </div>
       <input
         style={{
           margin: 0,
